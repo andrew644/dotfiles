78b9dc093eb68f24852aaf9bc314a07bdd594e11
andrew644 <andrew644@users.noreply.github.com>
2019-07-21 23:26:08 -0400

    Add installer

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+ln -sf ~/dotfiles/.config/i3status/config ~/.config/i3status/config
+ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
+
+ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+ln -sf ~/dotfiles/.config/.xinitrc ~/.xinitrc
+
+ln -sf ~/dotfiles/.config/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.config/.zshrc ~/.zshrc
+
