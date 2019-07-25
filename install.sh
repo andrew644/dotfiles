08b41515ef8f4883b1a811786dfe5fbbaa2c27a8
andrew644 <andrew644@users.noreply.github.com>
2019-07-24 20:04:05 -0400

    Rename .config to config

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-ln -sf ~/dotfiles/.config/i3status/config ~/.config/i3status/config
-ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/config/i3status/config ~/.config/i3status/config
+ln -sf ~/dotfiles/config/i3/config ~/.config/i3/config
 
-ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
 
-ln -sf ~/dotfiles/.config/.xinitrc ~/.xinitrc
+ln -sf ~/dotfiles/config/.xinitrc ~/.xinitrc
 
-ln -sf ~/dotfiles/.config/zsh/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/config/zsh/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/config/zsh/.zshrc ~/.zshrc
 
