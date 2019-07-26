8bf06e95026a7d4cae70d989de1493dab4caea64
andrew644 <andrew644@users.noreply.github.com>
2019-07-26 00:53:19 -0400

    Switch to polybar with compton

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ ln -sf ~/dotfiles/config/zsh/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/config/zsh/.zshrc ~/.zshrc
 
 ln -sf ~/dotfiles/config/termite/config ~/.config/termite/config
+
+ln -sf ~/dotfiles/config/compton/compton.conf ~/.config/compton/compton.conf
+ln -sf ~/dotfiles/config/compton/start.sh ~/.config/compton/start.sh
+
+ln -sf ~/dotfiles/config/polybar/config ~/.config/polybar/config
+ln -sf ~/dotfiles/config/polybar/start.sh ~/.config/polybar/start.sh
