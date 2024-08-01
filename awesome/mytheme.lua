64a27bc55ba60be8e8db35171b5fc360357eee15
andrew644 <andrew644@users.noreply.github.com>
2024-08-01 17:40:40 -0400

    Kanagawa awesome theme

--- awesome/mytheme.lua
+++ awesome/mytheme.lua
@@ -7,24 +7,66 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "sans 8"
+theme.font          = "Meslo 10"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+-- Kanagawa colors
+local colors = {
+    fujiWhite = "#DCD7BA",
+    oldWhite = "#C8C093",
+    sumiInk0 = "#16161D",
+    sumiInk1 = "#1F1F28",
+    sumiInk2 = "#2A2A37",
+    sumiInk3 = "#363646",
+    sumiInk4 = "#54546D",
+    waveBlue1 = "#223249",
+    waveBlue2 = "#2D4F67",
+    winterGreen = "#2B3328",
+    winterYellow = "#49443C",
+    winterRed = "#43242B",
+    winterBlue = "#252535",
+    autumnGreen = "#76946A",
+    autumnRed = "#C34043",
+    autumnYellow = "#DCA561",
+    samuraiRed = "#E82424",
+    roninYellow = "#FF9E3B",
+    waveAqua1 = "#6A9589",
+    dragonBlue = "#658594",
+    fujiGray = "#727169",
+    springViolet1 = "#938AA9",
+    oniViolet = "#957FB8",
+    crystalBlue = "#7E9CD8",
+    springViolet2 = "#9CABCA",
+    springBlue = "#7FB4CA",
+    lightBlue = "#A3D4D5",
+    waveAqua2 = "#7AA89F",
+    springGreen = "#98BB6C",
+    boatYellow1 = "#938056",
+    boatYellow2 = "#C0A36E",
+    carpYellow = "#E6C384",
+    sakuraPink = "#D27E99",
+    waveRed = "#E46876",
+    peachRed = "#FF5D62",
+    surimiOrange = "#FFA066",
+    katanaGray = "#717C7C",
+}
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+-- Theme settings
+theme.bg_normal     = colors.sumiInk1   -- Default background
+theme.bg_focus      = colors.waveBlue2  -- Focused background
+theme.bg_urgent     = colors.samuraiRed -- Urgent background
+theme.bg_minimize   = colors.sumiInk3   -- Minimized background
+theme.bg_systray    = theme.bg_normal   -- Systray background
+
+theme.fg_normal     = colors.fujiGray   -- Default foreground
+theme.fg_focus      = colors.fujiWhite  -- Focused foreground
+theme.fg_urgent     = colors.fujiWhite  -- Urgent foreground
+theme.fg_minimize   = colors.fujiWhite  -- Minimized foreground
 
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+theme.border_normal = colors.sumiInk0   -- Normal border
+theme.border_focus  = colors.waveBlue2  -- Focused border
+theme.border_marked = colors.autumnRed  -- Marked border
 
 -- There are other variable sets
 -- overriding the default one when
