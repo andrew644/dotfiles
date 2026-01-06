1c0df68e375f2df323c5ae9ac73eecfe8ac04358
andrew644 <andrew644@users.noreply.github.com>
2026-01-06 01:08:06 -0500

    Add basic emacs config

---
+++ emacs/init.el
@@ -0,0 +1,60 @@
+;; Remove ui
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Package setup
+(require 'package)
+(setq package-archives
+	  '(
+		("melpa" . "https://melpa.org/packages/")
+		("gnu" . "https://elpa.gnu.org/packages/")
+		)
+	  )
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t
+		evil-want-keybinding nil
+		evil-want-C-u-scroll t
+		evil-want-C-i-jump nil
+		)
+  :config
+  (evil-mode 1)
+  )
+
+;; Theme
+(use-package tokyonight-themes
+  :vc (:url "https://github.com/xuchengpeng/tokyonight-themes")
+  :config
+  (load-theme 'tokyonight-moon :no-confirm))
+
+;; Settings
+(setq-default tab-width 4)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((tokyonight-themes :url
+			"https://github.com/xuchengpeng/tokyonight-themes"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
